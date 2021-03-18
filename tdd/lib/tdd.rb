387fef610307c59a64a9e6749df549b4f4721543
aturi <angelaturi99@gmail.com>
2021-03-18 14:43:35 -0400

    Add #two_sum and #my_transpose

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -10,4 +10,15 @@ class Array
         end
         new_arr
     end
+
+    def two_sum
+        new_arr = []
+        self.each_with_index do |num1, i1|
+            self.each_with_index do |num2, i2|
+                new_arr << [i1, i2] if i2 > i1 && num1 + num2 == 0
+            end
+        end
+
+        new_arr
+    end
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -9,14 +9,25 @@ require "tdd"
             end
         end
 
+        describe "two_sum" do
+        let(:array) { [-1, 0, 2, -2, 1] }
+
+            it "finds all pairs of positions where the elements at those positions sum to 0" do
+            expect(array.two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        describe "my_transpose" do 
+        let(:array) { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
+
+            it "transposes rows and columns" do
+                expect(array.my_transpose).to eq([0, 3, 6], [1, 4, 7], [2, 5, 8])
+            end
+        end
+
+
     end
-        # let(:array) { [1, 2, 1, 3, 3]}
-        # let(:uniq_arr){my_uniq(array.dup) }
-        # it "removes duplicates" do 
-        #     array.each do |ele|
-        #         expect(uniq_arr.count(ele)).to eq(1)
-        #     end
-        # end
+
+
 
     
 
