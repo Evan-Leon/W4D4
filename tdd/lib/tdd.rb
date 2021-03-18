75afc50d9a084e844dfdfdcaedcb84c0851b0a28
Evan-Leon <evan.leon.j@gmail.com>
2021-03-18 15:00:58 -0400

    Completed Array specs and stock_picker specs

--- tdd/lib/tdd.rb
+++ tdd/lib/tdd.rb
@@ -21,4 +21,28 @@ class Array
 
         new_arr
     end
+
+    def my_transpose
+       height = self.length 
+       width = self[0].length  
+       results = Array.new(height) {Array.new(width)}
+       (0...height).each do |row|
+        (0...width).each do |col|
+            results[col][row] = self[row][col]
+        end
+       end
+       results 
+    end
+end
+
+def stock_picker(arr)
+    max = arr.max 
+    min = arr.min 
+    ((arr.length - 1).downto(0)).each do |i1|
+        ((arr.length - 1).downto(0)).each do |i2|
+            if i2 > i1 
+                return [arr.index(min), arr.index(max)]
+            end
+        end
+    end
 end
--- tdd/spec/tdd_spec.rb
+++ tdd/spec/tdd_spec.rb
@@ -9,23 +9,31 @@ require "tdd"
             end
         end
 
-        describe "two_sum" do
-        let(:array) { [-1, 0, 2, -2, 1] }
+        describe "#two_sum" do
+            let(:array) { [-1, 0, 2, -2, 1] }
 
             it "finds all pairs of positions where the elements at those positions sum to 0" do
             expect(array.two_sum).to eq([[0, 4], [2, 3]])
-        end
-
-        describe "my_transpose" do 
-        let(:array) { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
-
-            it "transposes rows and columns" do
-                expect(array.my_transpose).to eq([0, 3, 6], [1, 4, 7], [2, 5, 8])
             end
         end
 
+        describe "#my_transpose" do 
+            let(:array) { [[0, 1, 2], [3, 4, 5], [6, 7, 8]] }
 
+            it "transposes rows and columns" do
+                expect(array.my_transpose).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+            end
+        end
     end
+
+    describe "#stock_picker" do  
+        let(:array) {[2, 5, 10, 4, 1, 100, 7]}   
+        it "returns array where first element is best day to buy, second ele is best day to sell" do
+            expect(stock_picker(array)).to eq([4, 5])
+        end
+    end
+
+    
 
 
 
