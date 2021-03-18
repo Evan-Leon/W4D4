3a7ad59cc9aea391e881705a2b28aad484ecae79
Evan-Leon <evan.leon.j@gmail.com>
2021-03-18 16:55:47 -0400

    Finished specs on tower_of_hanoi

--- tdd/lib/hanoi.rb
+++ tdd/lib/hanoi.rb
@@ -8,4 +8,20 @@ class TowerHanoi
         @discs = [[3, 2, 1], [], []]
     end
 
+    def move(from_pile, to_pile) 
+        # raise "NOT ALLOWED to move on smaller discs" unless valid_move?(from_pile, to_pile)
+        raise "can not move from an empty pile" if @discs[from_pile].empty?
+        @discs[to_pile] <<@discs[from_pile].pop
+    end
+
+    def won?
+        @discs[0].empty? && @discs[1..2].any?{|ele| ele.empty?}
+    end
+
+    def valid_move?(from_pile, to_pile)
+        arr = [from_pile, to_pile]
+        !@discs[from_pile].empty? && @discs[to_pile].empty? || @discs[two_pile][-1] > @discs[from_pile][-1]
+        return false if arr.none? {|i| i.between?(0,2)} 
+    end
+
 end
--- tdd/spec/hanoi_spec.rb
+++ tdd/spec/hanoi_spec.rb
@@ -8,13 +8,59 @@ describe TowerHanoi do
                 expect(game.discs.length).to eq(3)
             end
 
+            it "initializes TowerHanoi with 2 empty inner arrays" do 
+                expect(game.discs[1].empty?).to be true  
+                expect(game.discs[2].empty?).to be true  
+            end
         end
     end
 
     describe "#move" do
-        before(:each) { game.move } 
-        it "moves one disk to another disc pile" do
-            expect(game.discs[1]).to eq([1])
+        
+        it "can move on to larger disc" do 
+            game.move(0, 2)
+            game.move(0, 1)
+            expect {game.move(2, 1)}.not_to raise_error 
+        end
+
+        it "can not move from an empty pile" do 
+            expect{game.move(1, 2)}.to raise_error("can not move from an empty pile")
+        end
+    end
+
+    describe "#won?" do 
+        let(:winner) {}
+        
+        it "when all discs are moved to pile 1" do 
+            game.move(0, 1)
+            game.move(0, 2)
+            game.move(1, 2)
+            game.move(0, 1)
+            game.move(2, 0)
+            game.move(2, 1)
+            game.move(0, 1)
+
+            expect(game).to be_won
+        end
+
+         it "when all discs are moved to pile 2" do 
+            game.move(0, 2)
+            game.move(0, 1)
+            game.move(2, 1)
+            game.move(0, 2)
+            game.move(1, 0)
+            game.move(1, 2)
+            game.move(0, 2)
+
+            expect(game).to be_won
+        end
+    end
+
+    describe "#valid_move?" do 
+        let(:board) {double("board", valid_move?:true)}
+        
+        it "should return true if valid move and should return flase if not a valid move" do 
+            allow(board).to receive(:valid_move?).and_return(false)
         end
     end
 end
